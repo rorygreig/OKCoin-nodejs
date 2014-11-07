82a9da8d512d6d9a043686d87914be764c707028
rorygreig <rorygreig@gmail.com>
2014-11-07 16:26:04 +0000

    modify coinfloor wrapper code

--- OKCoin.js
+++ OKCoin.js
@@ -1,114 +1,133 @@
-var ws = require("ws");
+(function() {
+	var SocketClient = require('ws');
 
-var wsUri ="wss://real.okcoin.com:10440/websocket/okcoinapi"; 
-var output;
+	var url = "wss://real.okcoin.com:10440/websocket/okcoinapi";
 
-var isConnect =2 ;
-function init() {
-    output = document.getElementById("output");
-    testWebSocket();
-setInterval(checkConnect,5000);
-}
-function checkConnect() {
-    if(isConnect == 2)
-    {
-        testWebSocket()
-    }
-}
+	var ws = new SocketClient(url);
 
-function testWebSocket() {
-    websocket = new WebSocket(wsUri);
-    websocket.onopen = function(evt) {
-  isConnect=1;
-        onOpen(evt)
-    };
-    websocket.onclose = function(evt) {
-  isConnect = 2;
-        onClose(evt)
-    };
-    websocket.onmessage = function(evt) {
-       onMessage(evt)
-    };
-    websocket.onerror = function(evt) {
-        onError(evt)
-    };
+	var _event_handlers = Object();
+	var _tag = 1;
+	var _result_handlers = Object();
+	var _idle_ping_timer_id = null;
 
-}
+	module.exports = OKCoin = (function(){
+		function OKCoin(user_id, password, api_key, onConnect){
+			this.user_id = user_id;
+			this.password = password;
+			this.api_key = api_key;
 
-function onOpen(evt) {
-    writeToScreen("CONNECTED");
-doSend("{'event':'addChannel','channel':'ok_btcusd_ticker'}");
-doSend("{'event':'addChannel','channel':'ok_ltcusd_ticker'}");
-doSend("{'event':'addChannel','channel':'ok_btcusd_trades'}");
-doSend("{'event':'addChannel','channel':'ok_ltcusd_trades'}");
-doSend("{'event':'addChannel','channel':'ok_btcusd_depth'}");
-doSend("{'event':'addChannel','channel':'ok_ltcusd_depth'}");
-//doSend("{'event':'removeChannel','channel':'ok_ltcusd_depth'}");
-//doSend("{'event':'removeChannel','channel':'ok_btcusd_depth'}");
-}
+			/*
+			 * add authentication function to event handlers
+			 */
+			_event_handlers["Welcome"] =  function(msg){
+				console.log("Authenticating");
+				authenticate(user_id, password, api_key, msg.nonce, function(){
+					onConnect();
+				});
+			};
 
-function onClose(evt) {
-    writeToScreen("DISCONNECTED");
-}
+			/*
+			 * set up websocket connection
+			 */
+			ws.on('open', function(data){
+				console.log('websocket connected to: ' + url);
+			});
 
-function onMessage(evt) {
-    //writeToScreen('<span style="color: blue;">RESPONSE: '+ evt.data+'</span>');
-      var myList	= evt.data;
-      var array = JSON.parse(myList);
-      createTable(array);
-}
+			/*
+			 * On each message call the relevant event handler
+			 */
+			ws.on('message', function (data, flags) {
+				var msg = JSON.parse(data);
+				if(msg !== undefined){
+					console.log("\nReceived Message:")
+					if(msg.error_code !== undefined && msg.error_code > 0){
+						console.log('error: ');
+						console.log( msg );
+					} else {
+						//call result handler function based on tag
+						if(msg.tag !== undefined){
+							handleResult(msg);
+						}
 
-function onError(evt) {
-    writeToScreen('<span style="color: red;">ERROR:</span> '+ evt.data);
-}
+						//call event handler function if this is a notification
+						if(msg.notice !== undefined){
+							handleNotification(msg);
+						}
+					}
+				}
+			});
+		}
 
-function doSend(message) {
-    writeToScreen("SENT: " + message);
-    websocket.send(message);
-}
+		function _do_request(request, callback){
+			console.log("\nSending Request:")
+			console.log(request);
+			var tag = request.tag;
+			ws.send(JSON.stringify(request), function(err){ if(err) throw(err); });
+			_result_handlers[tag] = callback;
+			_reset_idle_ping_timer();
+			_tag++;
+		};
 
-function writeToScreen(message) {
-    var pre = document.createElement("p");
-    pre.style.wordWrap = "break-word";
-    pre.innerHTML = message;
-   // output.appendChild(pre);
+		function _reset_idle_ping_timer() {
+			if (_idle_ping_timer_id) {
+				clearTimeout(_idle_ping_timer_id);
+			}
+			_idle_ping_timer_id = setTimeout(function () {
+				console.log("\nSending ping request to keep connection open");
+				_do_request({ }, function () { });
+			}, 45000);
+		};
 
-}
+		function handleNotification(msg){
+			var handler = _event_handlers[msg.notice];
+			if(handler !== undefined){
+				handler(msg);
+			} else {
+				console.log("No handler function for event: '" + msg.notice + "'");
+			}
+		}
 
-window.addEventListener("load", init, false);
+		function handleResult(msg){
+			var handler = _result_handlers[msg.tag];
+			if(handler !== undefined && typeof(handler) === "function"){
+				handler(msg);
+				delete _result_handlers[msg.tag];
+			}
+		}
+
+		/*
+		 * Authenticates as the specified user with the given authentication cookie
+		 * and passphrase.
+		 */
+		function authenticate(user_id, password, cookie, callback) {
+
+			var request = {
+				  "tag": _tag,
+					"method": "Authenticate",
+					"user_id": Number(user_id),
+					"cookie": cookie,
+					"nonce": btoa(client_nonce),
+					"signature": [ btoa(signature.r), btoa(signature.s)]
+			};
+
+			_do_request(request, function(result){
+				console.log("Successfully authenticated user: " + user_id);
+				callback(result);
+			});
+		};
+
+		/*
+		* add a listener for a message notice, to be called when this
+		* message is received
+		*/
+		OKCoin.prototype.addEventListener = function(notice, handler){
+			_event_handlers[notice] = handler;
+		}
 
 
 
-function createTable(array){
-    //table header
-     var  str='<h2 id="th2">WebSocket Test</h2><table id="tdata" border="1"><tr id="tr1">';
-    for (var index in array[0]) {
-        str += '<th>' + index + '</th>';
-    }
-    str += '</tr><tr id="tr2">';
+		return OKCoin;
 
-    // table body
+	})();
 
-    for (var i = 0; i < array.length; i++) {
-
-      for (var index in array[i]) {
-        var temp=array[i][index];
-        str += '<td>';
-        var tem=JSON.stringify(temp);
-        str += tem;
-        str += '</td>';
-      }
-    str += '</tr>';
-    }
-
-  str += '</table>';
-  removeTable('tdata');
-   document.write(str);
-}
- function removeTable(id)
-{
-    var tbl = document.getElementById(id);
-    if(tbl) tbl.parentNode.removeChild(tbl);
-    var tt = document.getElementById('th2');
-    if(tt) tt.parentNode.removeChild(tt);
-}
+}).call(this);
